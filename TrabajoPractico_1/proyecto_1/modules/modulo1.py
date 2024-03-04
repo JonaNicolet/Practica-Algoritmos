3e54cef349a6d5322ea423475efbbb760a8f4105
JonaNicolet <jonathan.nicolet@uner.edu.ar>
2024-03-04 15:05:31 -0300

    Agrego la funcion fibo

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,1 +1,10 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
+
+def fibo(n):
+    if n in {0,1}:
+        return n
+    else:
+        return fibo(n-1) + fibo(n-2)
+    
+
+
